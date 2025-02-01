4bd0968e0fa77252d341b1cfa0b9ac896081d574
Baxtiyor Shamshiddinov <baxtiyor.shamshiddinov@mail.ru>
2025-02-01 16:40:07 +0500

    carusel amaliyot 1

---
+++ index.js
@@ -0,0 +1,11 @@
+const elImgs = document.getElementById("imgs");
+const elLeftBtn = document.getElementById("left");
+const elRightBtn = document.getElementById("right");
+const elImg = document.querySelectorAll("#imgs img");
+
+idx = 0
+elRightBtn.addEventListener("click", changeImage()){
+  idx++;
+  elImgs.
+}
+
