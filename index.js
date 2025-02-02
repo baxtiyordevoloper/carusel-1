7a61a27204df212510f03e562af17bc4069516c6
Baxtiyor Shamshiddinov <baxtiyor.shamshiddinov@mail.ru>
2025-02-02 10:34:24 +0500

    index.js fayli to'liq yozildi and style.css fayliga ham o'zgartirish kiritildi orqa fonga img qo'ydik

--- index.js
+++ index.js
@@ -3,9 +3,37 @@ const elLeftBtn = document.getElementById("left");
 const elRightBtn = document.getElementById("right");
 const elImg = document.querySelectorAll("#imgs img");
 
-idx = 0
-elRightBtn.addEventListener("click", changeImage()){
-  idx++;
-  elImgs.
+let idx = 0;
+function changeImage(){
+  if(idx > elImg.length -1){
+    idx = 0;
+  } else if(idx < 0){
+    idx = elImg.length -1;
+  }
+  elImgs.style.transform = `translateX(${-idx * 500}px)`;
 }
 
+elRightBtn.addEventListener('click', function change(){
+  idx++;
+  changeImage();
+  resetInterval();
+});
+elLeftBtn.addEventListener('click', function change(){
+  idx--;
+  changeImage();
+  resetInterval();
+});
+
+let interval = setInterval(run, 3000);
+
+function run(){
+  idx++;
+  changeImage();
+  
+}
+
+function resetInterval(){
+  clearInterval(interval);
+
+  interval = setInterval(run,3000);
+}
